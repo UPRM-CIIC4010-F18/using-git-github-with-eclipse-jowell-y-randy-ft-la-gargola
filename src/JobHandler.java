fe634adbd84ccb4955a41507c5da95c741e22597
4010g12 <4010g12@corazon.ece.uprm.edu>
2018-08-29 11:09:39 -0400

    Promotion

--- src/JobHandler.java
+++ src/JobHandler.java
@@ -77,8 +77,25 @@ public class JobHandler {
 		if(!availability){ return qualify;}
 		if(eRank <= 0) { return qualify;}
 		
-		//Add Code Here [You have to use Switch and If/Else to get graded]
+		switch(seniority){
+		case 1:
+			eRank += 5;
+			break;
+		case 2:
+			eRank += 10;
+			break;
+		case 3:
+			eRank += 20;
+			break;
+		default:
+			eRank += 0;
+			break;	
+		}
 		
+		if(eRank >= jobRank || !(jobType)){
+			qualify = true;
+		}
+	
 		return qualify; 
 	}
 }
